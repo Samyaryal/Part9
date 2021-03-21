febbe173f82cc6803d35834b8619a720e1530500
Samikshya <samikshyaaryal5@gmail.com>
2021-03-21 19:09:17 +0200

    part9 exercise 9.6 webBMI

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 
 app.use(express.json());
@@ -7,6 +8,18 @@ app.get('/hello', (_req,res) => {
   res.send('Hello Full Stack');
 } );
 
+
+app.get('/bmi', (req, res) => {
+  const { height, weight} = req.body;
+  
+  if(isNaN(height) || isNaN(weight)){
+    res.status(400).json({error: " malformated parameters"});
+  }
+  else {
+    const result = {height: height, weight:weight, bmi:calculateBmi(height, weight)}
+    res.json(result)
+  }
+})
 
 const PORT = 3003;
 
