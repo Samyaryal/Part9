cf4eae69dc714dacb5a39be91eea4ecdc702e6a9
Samikshya <samikshyaaryal5@gmail.com>
2021-03-21 19:35:43 +0200

    part9 exercise 9.6 and 9.7

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express = require("express");
 import { calculateBmi } from "./bmiCalculator";
+import {calculateExercises} from "./exercisecalculator";
 const app = express();
 
 app.use(express.json());
@@ -19,6 +20,22 @@ app.get('/bmi', (req, res) => {
     const result = {height: height, weight:weight, bmi:calculateBmi(height, weight)}
     res.json(result)
   }
+})
+
+app.post('/exercise', (req, res) => {
+  const {daily_exercises , target } : {daily_exercises: number[], target:number} = req.body;
+
+  if (daily_exercises.length ==0 || !target){
+    res.json({'error': 'parameters missing'});
+    return;
+  }
+  if (!daily_exercises.every(a => !isNaN(a)) || isNaN(target)){
+    res.json({'error': 'malformatted parameters'});
+    return;
+  }
+  const result  = calculateExercises(daily_exercises, target);
+  res.json(result);
+
 })
 
 const PORT = 3003;
